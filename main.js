617530dbb70fb09c94d4db7b5b2ed47be5baeadd
Vladimir-Beliy <vovaazick@gmail.com>
2020-02-09 21:50:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+let input = document.getElementsByClassName('input')[0];
+let output = document.getElementsByClassName('output')[0];
+
+input.addEventListener('keyup', function(){
+    output.innerHTML = convertToRoman(this.value);
+}, false);
+
+
+var convertToRoman = function(num) {
+    var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var romanNumeral = [
+      "M",
+      "CM",
+      "D",
+      "CD",
+      "C",
+      "XC",
+      "L",
+      "XL",
+      "X",
+      "IX",
+      "V",
+      "IV",
+      "I"
+    ];
+  
+    var romanized = "";
+  
+    for (var index = 0; index < decimalValue.length; index++) {
+      while (decimalValue[index] <= num) {
+        romanized += romanNumeral[index];
+        num -= decimalValue[index];
+      }
+    }
+  
+    return romanized;
+  };
